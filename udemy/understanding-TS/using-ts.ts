15f15b8e5423472737a55c6deb28a267a0766ca6
MoMoZin <khinmomozin@gmail.com>
2023-06-30 13:55:52 -0600

    js type check

--- udemy/understanding-TS/using-ts.ts
+++ udemy/understanding-TS/using-ts.ts
@@ -3,13 +3,7 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  // Javascript way of making sure type conversion
-  if (typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 + num2;
-  }
-  else {
-    return +num1 + +num2;
-  }
+  return num1 + num2;
 }
 
 button.addEventListener("click", function() {
